ce1b4eab9b289aee2870b3d8830703abe91751cc
Mathis <mathis.louarn@etu.univ-amu.fr>
2023-06-02 14:44:31 +0200

    correction d'erreur lorsque on appuis plusieurs fois sur le bouton tracer sans vider entre deux clics

--- src/main/java/fr/amu/iut/cc3/ToileController.java
+++ src/main/java/fr/amu/iut/cc3/ToileController.java
@@ -34,10 +34,18 @@ public class ToileController implements Initializable {
     @FXML
     Button boutonVider;
     @FXML
-    Pane graph;
+    Pane toile;
+
+    Circle c1 = new Circle();
+    Circle c2 = new Circle();
+    Circle c3 = new Circle();
+    Circle c4 = new Circle();
+    Circle c5 = new Circle();
+    Circle c6 = new Circle();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        toile.getChildren().addAll(c1,c2,c3,c4,c5,c6);
     }
 
     int getXRadarChart(double value, int axe){
@@ -51,18 +59,32 @@ public class ToileController implements Initializable {
     }
     @FXML
     public void tracer(){
-        Circle c1 = new Circle(getXRadarChart(Integer.parseInt(comp1.getText()),1),getYRadarChart(Integer.parseInt(comp1.getText()),1), 5);
-        Circle c2 = new Circle(getXRadarChart(Integer.parseInt(comp2.getText()),2),getYRadarChart(Integer.parseInt(comp2.getText()),2), 5);
-        Circle c3 = new Circle(getXRadarChart(Integer.parseInt(comp3.getText()),3),getYRadarChart(Integer.parseInt(comp3.getText()),3), 5);
-        Circle c4 = new Circle(getXRadarChart(Integer.parseInt(comp4.getText()),4),getYRadarChart(Integer.parseInt(comp4.getText()),4), 5);
-        Circle c5 = new Circle(getXRadarChart(Integer.parseInt(comp5.getText()),5),getYRadarChart(Integer.parseInt(comp5.getText()),5), 5);
-        Circle c6 = new Circle(getXRadarChart(Integer.parseInt(comp6.getText()),5),getYRadarChart(Integer.parseInt(comp6.getText()),6), 5);
-        graph.getChildren().addAll(c1,c2,c3,c4,c5,c6);
+        vider();
+        c1.setCenterX(getXRadarChart(Integer.parseInt(comp2.getText()),1));
+        c1.setCenterY(getYRadarChart(Integer.parseInt(comp2.getText()),1));
+        c1.setRadius(5);
+        c2.setCenterX(getXRadarChart(Integer.parseInt(comp2.getText()),2));
+        c2.setCenterY(getYRadarChart(Integer.parseInt(comp2.getText()),2));
+        c2.setRadius(5);
+        c3.setCenterX(getXRadarChart(Integer.parseInt(comp3.getText()),3));
+        c3.setCenterY(getYRadarChart(Integer.parseInt(comp3.getText()),3));
+        c3.setRadius(5);
+        c4.setCenterX(getXRadarChart(Integer.parseInt(comp4.getText()),4));
+        c4.setCenterY(getYRadarChart(Integer.parseInt(comp4.getText()),4));
+        c4.setRadius(5);
+        c5.setCenterX(getXRadarChart(Integer.parseInt(comp5.getText()),5));
+        c5.setCenterY(getYRadarChart(Integer.parseInt(comp5.getText()),5));
+        c5.setRadius(5);
+        c6.setCenterX(getXRadarChart(Integer.parseInt(comp6.getText()),6));
+        c6.setCenterY(getYRadarChart(Integer.parseInt(comp6.getText()),6));
+        c6.setRadius(5);
+
+        toile.getChildren().addAll(c1,c2,c3,c4,c5,c6);
     }
 
     @FXML
     public void vider(){
-        graph.getChildren().clear();
+        toile.getChildren().clear();
     }
 
 }
